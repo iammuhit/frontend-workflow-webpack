6aa5f5b6ab52fb2a152b08c052363a55ca821871
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2022-01-26 00:36:21 +0100

    Update Constants

--- core/config/constants.js
+++ core/config/constants.js
@@ -11,11 +11,12 @@ export const NODE_ENV = ([ENV_DEVELOPMENT, ENV_PRODUCTION].includes(process.env.
 export const APP_MODE    = process.env.APP_MODE    = process.env.NODE_ENV = NODE_ENV ?? ENV_DEVELOPMENT;
 export const APP_VERSION = process.env.APP_VERSION = env.APP_VERSION ?? process.env.npm_package_version;
 
-export const PATH_BASE        = path.resolve(path.dirname(path.dirname(__dirname)));
-export const PATH_CORE        = path.resolve(PATH_BASE, 'core');
-export const PATH_APP         = path.resolve(PATH_BASE, 'src');
-export const PATH_COMPONENTS  = path.resolve(PATH_BASE, 'src/components');
-export const PATH_RESOURCES   = path.resolve(PATH_BASE, 'src/resources');
-export const PATH_ASSETS      = path.resolve(PATH_BASE, 'src/assets');
-export const PATH_DIST        = path.resolve(PATH_BASE, env.PATH_DIST ?? 'public');
-export const PATH_DIST_ASSETS = path.resolve(PATH_BASE, env.PATH_DIST ?? 'public/assets');
+export const PATH_BASE         = path.resolve(path.dirname(path.dirname(__dirname)));
+export const PATH_NODE_MODULES = path.resolve(PATH_BASE, 'node_modules');
+export const PATH_CORE         = path.resolve(PATH_BASE, 'core');
+export const PATH_APP          = path.resolve(PATH_BASE, 'src');
+export const PATH_COMPONENTS   = path.resolve(PATH_BASE, 'src/components');
+export const PATH_RESOURCES    = path.resolve(PATH_BASE, 'src/resources');
+export const PATH_ASSETS       = path.resolve(PATH_BASE, 'src/assets');
+export const PATH_DIST         = path.resolve(PATH_BASE, env.PATH_DIST ?? 'public');
+export const PATH_DIST_ASSETS  = path.resolve(PATH_BASE, env.PATH_DIST ?? 'public/assets');
