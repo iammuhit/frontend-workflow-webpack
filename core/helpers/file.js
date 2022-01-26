1c0ac1d5b134fd0a8b44445d4ce186b84b25002c
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2022-01-26 02:58:44 +0100

    Update File Helper

--- core/helpers/file.js
+++ core/helpers/file.js
@@ -3,14 +3,24 @@ import fsExtra from 'fs-extra';
 
 export const dirWalker = (dir, ext = '.twig') => {
     let files = [];
-    
+
     fsExtra.readdirSync(dir).forEach(filename => {
         const file = path.join(dir, filename);
         const status = fsExtra.statSync(file);
 
-        if(status && status.isDirectory() && path.basename(file).indexOf('_') !== 0) {
+        if(
+            status
+            && status.isDirectory()
+            && path.basename(file) !== 'blocks'
+            && path.basename(file).indexOf('_') !== 0
+        ) {
             files = files.concat(dirWalker(file));
-        } else if(status && !status.isDirectory() && path.extname(file) === ext && path.basename(file).indexOf('_') !== 0) {
+        } else if(
+            status
+            && !status.isDirectory()
+            && path.extname(file) === ext
+            && path.basename(file).indexOf('_') !== 0
+        ) {
             files.push(file);
         }
     });
