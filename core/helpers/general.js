f7ee413d7a0820ad8101cccad4e6c056bbe4fb3f
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2022-01-26 02:57:55 +0100

    Create URL helper

--- core/helpers/general.js
+++ core/helpers/general.js
@@ -11,6 +11,19 @@ export const env = (key, value) => {
     return res;
 };
 
+export const url = {
+    build: (base_url, uri = '', query = {}, fragment = '') => {
+        let url = typeof base_url !== 'undefined' ? base_url : '/';
+        let query_string = Object.keys(query).map((key) => [key, query[key]].join('=')).join('&');
+    
+        url = uri.length ? url + uri : url;
+        url = query_string.length ? [url, query_string].join('?') : url;
+        url = fragment.length ? [url, fragment].join('#') : url;
+    
+        return url;
+    },
+};
+
 export const banner = (banner, data) => new Template(banner)(data);
 
-export default { env, banner };
+export default { env, url, banner };
