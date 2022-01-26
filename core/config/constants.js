e75af9fb1bee7c29fa1d2832e01402dd0d87b676
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2022-01-26 01:16:00 +0100

    Update Constants

--- core/config/constants.js
+++ core/config/constants.js
@@ -5,11 +5,13 @@ const env = dotenv.config().parsed;
 
 export const ENV_DEVELOPMENT = process.env.ENV_DEVELOPMENT = 'development';
 export const ENV_PRODUCTION  = process.env.ENV_PRODUCTION  = 'production';
+export const ENV_LANG_EN     = process.env.ENV_LANG        = 'en';
 
 export const NODE_ENV = ([ENV_DEVELOPMENT, ENV_PRODUCTION].includes(process.env.NODE_ENV)) ? process.env.NODE_ENV : env.APP_MODE;
 
 export const APP_MODE    = process.env.APP_MODE    = process.env.NODE_ENV = NODE_ENV ?? ENV_DEVELOPMENT;
 export const APP_VERSION = process.env.APP_VERSION = env.APP_VERSION ?? process.env.npm_package_version;
+export const APP_LANG    = process.env.APP_LANG    = env.APP_LANG ?? ENV_LANG_EN;
 
 export const PATH_BASE         = path.resolve(path.dirname(path.dirname(__dirname)));
 export const PATH_NODE_MODULES = path.resolve(PATH_BASE, 'node_modules');
