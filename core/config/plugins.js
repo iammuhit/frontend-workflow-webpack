e1d277ae5032fc73eeecdf70d853a3e84416bb17
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2022-01-27 06:37:53 +0100

    Update Favicons Plugin

--- core/config/plugins.js
+++ core/config/plugins.js
@@ -106,7 +106,27 @@ export const workbox = {
 };
 
 export const favicons = {
-    logo: path.resolve(constants.PATH_ASSETS, 'img/favicon.png'),
-    prefix: 'img/favicons/',
-    cache: true
+    logo    : path.resolve(constants.PATH_ASSETS, 'img/favicon.png'),
+    prefix  : 'img/favicons/',
+    cache   : constants.APP_MODE === constants.ENV_PRODUCTION,
+    mode    : env('FAVICONS_MODE', 'auto'),
+    inject  : env('FAVICONS_INJECT', false),
+    favicons: {
+        appName       : env('APP_NAME', ''),
+        appDescription: env('APP_DESCRIPTION', ''),
+        developerName : env('COMPANY_NAME', ''),
+        developerURL  : env('COMPANY_WEBSITE', ''),
+        background    : env('FAVICONS_BACKGROUND', '#ddd'),
+        theme_color   : env('FAVICONS_THEME_COLOR', '#333'),
+        version       : env('APP_VERSION', ''),
+        icons         : {
+            favicons    : env('FAVICONS_ICONS', true),
+            android     : env('FAVICONS_ANDROID', false),
+            appleIcon   : env('FAVICONS_APPLE_ICON', false),
+            appleStartup: env('FAVICONS_APPLE_STARTUP', false),
+            windows     : env('FAVICONS_WINDOWS', false),
+            coast       : env('FAVICONS_COAST', false),
+            yandex      : env('FAVICONS_YANDEX', false),
+        },
+    },
 };
