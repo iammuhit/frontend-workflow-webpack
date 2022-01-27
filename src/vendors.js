5c0f8991734e3a6d108ac3795cedecc68e3b16a7
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2022-01-27 09:48:29 +0100

    Update Tailwind & Postcss Settings

--- src/vendors.js
+++ src/vendors.js
@@ -3,4 +3,5 @@
 import '@mayarun/vendors/jquery';
 import '@mayarun/vendors/bootstrap';
 import '@mayarun/vendors/fontawesome';
+// import '@mayarun/vendors/tailwindcss';
 // import '@mayarun/vendors/fontello';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ['./src/**/*.{html,js}'],
+    content: ['./src/**/*.{twig,html,js}'],
     darkMode: false, // media, class, false
     theme: {
         extend: {}
