0d68866c7c6c94e0ae3f0d97998494871d583581
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2022-01-28 12:20:59 +0100

    Update Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    purge: {
+        enabled: process.env.APP_MODE === process.env.ENV_PRODUCTION,
+        content: ['./public/**/*.{html,js]'],
+    },
     content: ['./src/**/*.{twig,html,js}'],
     darkMode: false, // media, class, false
     theme: {
