33cda5452a1155cd0e12de626e8c7c16f923b6e8
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2022-01-29 11:18:46 +0100

    update save-remote-file plugin

--- core/config/webpack-plugins/save-remote-file.js
+++ core/config/webpack-plugins/save-remote-file.js
@@ -1,7 +1,11 @@
 import path from 'path';
+import fs from 'fs-extra';
 import SaveRemoteFileWebpackPlugin from 'save-remote-file-webpack-plugin';
 import $ from '../../libraries/Loader';
 
 const constants = $.config('constants');
 
-export default new SaveRemoteFileWebpackPlugin(require(path.resolve(constants.PATH_APP, 'config/save-remote-file')));
+const userOptionsFile = path.resolve(constants.PATH_APP, 'config/save-remote-file.js');
+const userOptions = fs.existsSync(userOptionsFile) ? require(userOptionsFile) : [];
+
+export default new SaveRemoteFileWebpackPlugin(userOptions);
