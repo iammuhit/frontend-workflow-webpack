db81eb921bad08e877da82cfbbf68d6b58d0bc2f
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2022-02-08 06:43:54 +0100

    update clean plugin

--- core/config/wp-plugins/clean.js
+++ core/config/wp-plugins/clean.js
@@ -1,14 +1,15 @@
 import path from 'path';
+import fs from 'fs-extra';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import $ from '../../libraries/Loader';
 
 const constants = $.config('constants');
 const { env }   = $.helper('general');
 
-export default new CleanWebpackPlugin({
+const defaultOptions = {
     dry: false,
     verbose: env('APP_MODE') !== constants.ENV_PRODUCTION,
-    cleanStaleWebpackAssets: true,
+    cleanStaleWebpackAssets: false,
     protectWebpackAssets: true,
     cleanOnceBeforeBuildPatterns: [
         path.resolve(constants.PATH_DIST, '*'),
@@ -16,4 +17,15 @@ export default new CleanWebpackPlugin({
     ],
     cleanAfterEveryBuildPatterns: [],
     dangerouslyAllowCleanPatternsOutsideProject: false,
-});
+};
+
+if (env('SERVER_PROXY', false)) {
+    defaultOptions.cleanAfterEveryBuildPatterns.push(path.resolve(process.env.PATH_BASE, process.env.THEME_PATH, 'assets/**/*.map'));
+    defaultOptions.dangerouslyAllowCleanPatternsOutsideProject = true;
+}
+
+const userOptionsFile = path.resolve(constants.PATH_APP, 'config/clean.js');
+const userOptions = fs.existsSync(userOptionsFile) ? require(userOptionsFile) : {};
+const options = Object.keys(defaultOptions).reduce((a, key) => ({ ...a, [key]: userOptions[key] }), {});
+
+export default new CleanWebpackPlugin(defaultOptions);
